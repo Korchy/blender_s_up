5954a7292bc218e3dd87b6646b9c15997867889d
Nikita Akimov <korchiy@yandex.ru>
2019-06-14 23:00:46 +0300

    update

--- addon.py
+++ addon.py
@@ -17,6 +17,7 @@ class Addon:
     def dev_mode():
         rez = False
         conf_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), __class__.cfg_file_name)
+        print(conf_file_path)
         if os.path.exists(conf_file_path):
             with open(conf_file_path) as conf_file:
                 json_data = json.load(conf_file)
