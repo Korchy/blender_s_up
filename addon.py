259990b59871550b9ad9177756ccefc8728ada54
Nikita Akimov <korchiy@yandex.ru>
2019-06-14 23:05:29 +0300

    update

--- addon.py
+++ addon.py
@@ -17,12 +17,10 @@ class Addon:
     def dev_mode():
         rez = False
         conf_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), __class__.cfg_file_name)
-        print(conf_file_path)
         if os.path.exists(conf_file_path):
             with open(conf_file_path) as conf_file:
                 json_data = json.load(conf_file)
-                print('json', json_data)
-                if 'dev_status' in json_data and json_data['dev_status']:
+                if 'dev_mode' in json_data and json_data['dev_mode']:
                     rez = True
                 conf_file.close()
         return rez
